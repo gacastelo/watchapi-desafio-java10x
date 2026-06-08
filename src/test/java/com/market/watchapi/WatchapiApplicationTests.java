49ebaa2ba5f46f97cdc176055282589b4f209f51
gacastelo <gabriel.castelo5949@gmail.com>
2026-06-08 08:43:15 -0300

    initial commit

---
+++ src/test/java/com/market/watchapi/WatchapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market.watchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

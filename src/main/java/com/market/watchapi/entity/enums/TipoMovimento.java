ae21c1db71cd7608b1e40835601ffd1dd56ff857
gacastelo <gabriel.castelo5949@gmail.com>
2026-06-08 08:38:24 -0300

    Feat: Add Enums and Relogio Entity

---
+++ src/main/java/com/market/watchapi/entity/enums/TipoMovimento.java
@@ -0,0 +1,24 @@
+package com.market.watchapi.entity.enums;
+
+public enum TipoMovimento {
+    QUARTZ, AUTOMATIC, MANUAL;
+
+    public static TipoMovimento fromApi(String value) {
+        if(value == null || value.isBlank()) return null;
+
+        return switch (value){
+            case "quartz" -> QUARTZ;
+            case "automatic" -> AUTOMATIC;
+            case "manual" -> MANUAL;
+            default -> throw  new IllegalArgumentException("Tipo de Movimento Inválido: " + value);
+        };
+    }
+
+    public String toApi() {
+        return switch (this){
+            case QUARTZ -> "quartz";
+            case AUTOMATIC -> "automatic";
+            case MANUAL -> "manual";
+        };
+    }
+}

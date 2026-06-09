1d3858f4a4b00d2a35a06c06eec628df3e6f9b90
gacastelo <gabriel.castelo5949@gmail.com>
2026-06-08 22:49:34 -0300

    Feat: add Seed for initialization

---
+++ src/main/java/com/market/watchapi/config/CarregadorDadosInicial.java
@@ -0,0 +1,92 @@
+package com.market.watchapi.config;
+
+import com.market.watchapi.entity.Relogio;
+import com.market.watchapi.entity.enums.MaterialCaixa;
+import com.market.watchapi.entity.enums.TipoMovimento;
+import com.market.watchapi.entity.enums.TipoVidro;
+import com.market.watchapi.repository.RelogioRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+@Configuration
+public class CarregadorDadosInicial {
+
+    private final RelogioRepository relogioRepository;
+
+    public CarregadorDadosInicial(RelogioRepository relogioRepository) {
+        this.relogioRepository = relogioRepository;
+    }
+
+    @Bean
+    CommandLineRunner seedRelogios(){
+        return args -> {
+          if (!relogioRepository.findAll().isEmpty()) return;
+
+          Instant agora = Instant.now();
+            List<Relogio> relogios = List.of(
+                    Relogio.builder()
+                            .id(UUID.randomUUID())
+                            .marca("CasteloWatch")
+                            .modelo("F-10xT")
+                            .referencia("1234")
+                            .tipoMovimento(TipoMovimento.QUARTZ)
+                            .materialCaixa(MaterialCaixa.TITANIUM)
+                            .tipoVidro(TipoVidro.SAPPHIRE)
+                            .resistenciaAguaM(30)
+                            .diametroMn(35)
+                            .lugToLugMm(38)
+                            .espessuraMm(9)
+                            .larguraLugMm(18)
+                            .precoEmCentavos(18880)
+                            .urlImagem("url123")
+                            .createdAt(agora.minusSeconds(50000))
+                            .updatedAt(agora.minusSeconds(50000))
+                            .build(),
+                    Relogio.builder()
+                            .id(UUID.randomUUID())
+                            .marca("Seiko")
+                            .modelo("G-123cT")
+                            .referencia("5678")
+                            .tipoMovimento(TipoMovimento.MANUAL)
+                            .materialCaixa(MaterialCaixa.STEEL)
+                            .tipoVidro(TipoVidro.MINERAL)
+                            .resistenciaAguaM(200)
+                            .diametroMn(40)
+                            .lugToLugMm(40)
+                            .espessuraMm(12)
+                            .larguraLugMm(16)
+                            .precoEmCentavos(25080)
+                            .urlImagem("url124")
+                            .createdAt(agora.minusSeconds(23000))
+                            .updatedAt(agora.minusSeconds(23000))
+                            .build(),
+                    Relogio.builder()
+                            .id(UUID.randomUUID())
+                            .marca("Casio")
+                            .modelo("C-ter23")
+                            .referencia("8888")
+                            .tipoMovimento(TipoMovimento.AUTOMATIC)
+                            .materialCaixa(MaterialCaixa.CERAMIC)
+                            .tipoVidro(TipoVidro.MINERAL)
+                            .resistenciaAguaM(199)
+                            .diametroMn(40)
+                            .lugToLugMm(40)
+                            .espessuraMm(13)
+                            .larguraLugMm(16)
+                            .precoEmCentavos(15998)
+                            .urlImagem("url124")
+                            .createdAt(agora.minusSeconds(23400))
+                            .updatedAt(agora.minusSeconds(23400))
+                            .build()
+            );
+            relogioRepository.saveAll(relogios);
+        };
+    }
+
+
+}
